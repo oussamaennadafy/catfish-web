8dd3c7d3d3798ff04f151bf4c851f74df3986d60
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-06-15 00:36:33 +0100

    fix: update production URL to use https protocol in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const PROD_URL = "http://ec2-44-204-42-93.compute-1.amazonaws.com";
+const PROD_URL = "https://ec2-44-204-42-93.compute-1.amazonaws.com";
 const DEV_URL = "http://localhost:3000";
 
 const BASE_URLS = {
